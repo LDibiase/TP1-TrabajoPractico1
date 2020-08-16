b6e340890d97739ddab074cc90528576a74dae1a
Lucas Di Biase <lucas.dibiase@mercadolibre.com>
2020-08-16 20:05:44 -0300

    Ejercicio 1

---
+++ src/main/java/tp1/Main.java
@@ -0,0 +1,43 @@
+package tp1;
+
+import tp1.domain.Grupo;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Grupo grupo = new Grupo("2");
+
+		agregarIntegrante("Ornella", grupo);
+		agregarIntegrante("Lucas", grupo);
+		agregarIntegrante("Mariano", grupo);
+		agregarIntegrante("Andres", grupo);
+		agregarIntegrante("Alejandro", grupo);
+		agregarIntegrante("Fantasma", grupo);
+
+		probarExistencia(grupo, "Alejandro");
+
+		grupo.mostrar();
+
+		System.out.println("Se removió al participante " + grupo.removerIntegrante("Fantasma"));
+		grupo.removerIntegrante("Fantasma");
+
+		grupo.mostrar();
+		grupo.vaciar();
+		grupo.mostrar();
+	}
+
+	private static void agregarIntegrante(String nombre, Grupo grupo) {
+		grupo.agregarIntegrante(nombre);
+		System.out.println(nombre + " fue asignado al grupo " + grupo.getNombre());
+	}
+
+	private static void probarExistencia(Grupo grupo, String nombreABuscar) {
+		String nombre = grupo.buscarIntegrante(nombreABuscar);
+
+		if (nombre != null) {
+			System.out.println("El integrante fue agregado con éxito");
+		} else {
+			System.out.println("El integrante no fue agregado");
+		}
+	}
+}
---
+++ src/main/java/tp1/domain/Grupo.java
@@ -0,0 +1,125 @@
+package tp1.domain;
+
+public class Grupo {
+	private String nombre;
+	private String[] integrantes;
+
+	public Grupo(String nombre) {
+		this.nombre = nombre;
+		this.integrantes = new String[6];
+	}
+
+	public String getNombre() {
+		return this.nombre;
+	}
+
+	private void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public int getCantidadIntegrantes() {
+		int result = 0;
+
+		for (String nombre : this.integrantes) {
+			if (nombre != null) {
+				result++;
+			}
+		}
+
+		return result;
+	}
+
+	public void agregarIntegrante(String nombreIntegrante) {
+		if (!existeIntegrante(nombreIntegrante)) {
+			for (int i=0; i<this.integrantes.length; i++) {
+				if(this.integrantes[i] == null) {
+					this.integrantes[i] = nombreIntegrante;
+				}
+			}
+		}
+	}
+
+	private boolean existeIntegrante(String nombreIntegrante) {
+		int i = 0;
+		boolean result = false;
+
+		while (i < this.integrantes.length && !result) {
+			if (this.integrantes[i].equalsIgnoreCase(nombreIntegrante)) {
+				result = true;
+			}
+			i++;
+		}
+		return result;
+	}
+
+	private int obtenerPosicionIntegrante(String nombreIntegrante) {
+		int numInt = -1;
+		int i=0;
+
+		while (i < integrantes.length && !integrantes[i].equalsIgnoreCase(nombreIntegrante)) {
+			i++;
+		}
+
+		if (i < integrantes.length) {
+			numInt = i;
+		}
+
+		return  numInt;
+	}
+
+	public String obtenerIntegrante(int posicion) {
+		String result = null;
+
+		if (!existeIntegrante(this.integrantes[posicion])) {
+			return result;
+		}
+		return result = this.integrantes[posicion - 1];
+	}
+
+	public String buscarIntegrante(String nombre) {
+		String result = null;
+		int posicion;
+
+		posicion = obtenerPosicionIntegrante(nombre);
+
+		if (posicion != -1) {
+			result = this.integrantes[posicion];
+		}
+
+		return result;
+	}
+
+	public String removerIntegrante(String nombreIntegrante) {
+		int posicion;
+		String borrado = "";
+
+		posicion = obtenerPosicionIntegrante(nombreIntegrante);
+
+		if (posicion != -1) {
+			borrado = this.integrantes[posicion];
+			this.integrantes[posicion] = null;
+		}
+
+		return borrado;
+	}
+
+	private void mostrarIntegrantes() {
+		System.out.println("La cantidad de integrantes es: " + this.getCantidadIntegrantes());
+		for (int i = 0; i < this.integrantes.length; i++) {
+			if (this.integrantes[i] != null) {
+				System.out.println(this.integrantes[i]);
+			}
+		}
+	}
+
+	public void mostrar() {
+		System.out.println("El nombre del grupo es: " + this.getNombre());
+		this.mostrarIntegrantes();
+	}
+
+	public void vaciar() {
+		for (int i = 0; i < this.integrantes.length; i++) {
+			this.integrantes[i] = null;
+		}
+	}
+}
